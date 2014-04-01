2a560fd50ce4eebd3125e39765036669a8fa98aa
Holt Bowmer <bowmmh11@wfu.edu>
2014-04-01 00:37:38 -0400

    cleaned up bucket memory allocation

--- MillionSongs_Jaccard_Similarity.java
+++ MillionSongs_Jaccard_Similarity.java
@@ -27,177 +27,29 @@ public class MillionSongs_Jaccard_Similarity {
 		
 		
 
-		Vector[] bucket1 = new Vector[210524];
-		bucket1 = reduceVector(bucket1);
-		Vector[] bucket2 = new Vector[210524];
-		bucket2 = reduceVector(bucket2);
-		Vector[] bucket3 = new Vector[210524];
-		bucket3 = reduceVector(bucket3);
-		Vector[] bucket4 = new Vector[210524];
-		bucket4 = reduceVector(bucket4);
-		Vector[] bucket5 = new Vector[210524];
-		bucket5 = reduceVector(bucket5);
-		Vector[] bucket6 = new Vector[210524];
-		bucket6 = reduceVector(bucket6);
-		Vector[] bucket7 = new Vector[210524];
-		bucket7 = reduceVector(bucket7);
-		Vector[] bucket8 = new Vector[210524];
-		bucket8 = reduceVector(bucket8);
-		Vector[] bucket9 = new Vector[210524];
-		bucket9 = reduceVector(bucket9);
-		Vector[] bucket10 = new Vector[210524];
-		bucket10 = reduceVector(bucket10);
-		Vector[] bucket11 = new Vector[210524];
-		bucket11 = reduceVector(bucket11);
-		Vector[] bucket12 = new Vector[210524];
-		bucket12 = reduceVector(bucket12);
-		Vector[] bucket13 = new Vector[210524];
-		bucket13 = reduceVector(bucket13);
-		Vector[] bucket14 = new Vector[210524];
-		bucket14 = reduceVector(bucket14);
-		Vector[] bucket15 = new Vector[210524];
-		bucket15 = reduceVector(bucket15);
-		Vector[] bucket16 = new Vector[210524];
-		bucket16 = reduceVector(bucket16);
-		Vector[] bucket17 = new Vector[210524];
-		bucket17 = reduceVector(bucket17);
-		Vector[] bucket18 = new Vector[210524];
-		bucket18 = reduceVector(bucket18);
-		Vector[] bucket19 = new Vector[210524];
-		bucket19 = reduceVector(bucket19);
-		Vector[] bucket20 = new Vector[210524];
-		bucket20 = reduceVector(bucket20);
-		
-		
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(0, 4, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket1[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 1 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(5, 9, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket2[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 2 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(10, 14, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket3[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 3 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(15, 19, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket4[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 4 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(20, 24, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket5[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 5 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(25, 29, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket6[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 6 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(30, 34, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket7[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 7 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(35, 39, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket8[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 8 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(40, 44, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket9[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 9 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(45, 49, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket10[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 10 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(50, 54, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket11[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 11 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(55, 59, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket12[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 12 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(60, 64, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket13[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 13 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(65, 69, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket14[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 14 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(70, 74, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket15[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 15 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(75, 79, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket16[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 16 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(80, 84, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket17[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 17 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(85, 89, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket18[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 18 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(90, 94, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket19[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 19 done");
-		for(int i = 0; i < 210519; i++) {
-			int bucketValue = prependSignal(95, 99, i, sigArray);
-			bucketValue = Math.abs(bucketValue);
-			bucket20[bucketValue].addElement(i);
-		}
-		System.out.println("Bucket 20 done");
-		
-		for(int i = 0; i < 210519; i++) {
-			if(bucket1[i].size() > 1) {
-				for(int j = 1; j < bucket1[i].size(); j++) {
-					if(JacardSim(bucket1[i].get(j-1), JacardSim(bucket1[i].get(j), bitArray) == true) {
-						count++;
+		Vector<Integer>[] bucket = new Vector[210524];
+		bucket = reduceVector(bucket);
+		int temp = 0;
+		int[][] indices = new int [15000][2];
+		for(int i = 4; i < 100; i = i+5) {
+			bucket = fillBucket(temp, i, bucket, sigArray);
+			for(int k = 0; k < 210519; k++) {
+				if(bucket[k].size() > 1) {
+					if(checkIndices(indices, bucket[k].get(0), bucket[k].get(1))) {
+						for(int j = 1; j < bucket[k].size(); j++) {
+							if(JacardSim(bucket[k].get(j-1), bucket[k].get(j), bitArray) == true) {
+								indices[count][0] = bucket[k].get(j-1);
+								indices[count][1] = bucket[k].get(j);
+								count++;
+							}
+						}
 					}
 				}
 			}
+			for(int p = 0; p < 210523; p++) {
+				bucket[p].clear();
+			}
+			temp = i+1;
 		}
 	}
 	
@@ -205,9 +57,28 @@ public class MillionSongs_Jaccard_Similarity {
 	
 	public static Vector[] reduceVector(Vector[] v) {
 		for(int i = 0; i < 210519; i++) {
-			v[i] = new Vector(0);
+			v[i] = new Vector<>(0);
 		}
 		return v;
+	}
+	
+	public static boolean checkIndices(int[][] indexes, int smaller, int larger) {
+		for(int i = 0; i < 15000; i++) {
+			if((indexes[i][0] == smaller) && (indexes[i][1] == larger)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static Vector[] fillBucket(int start, int end, Vector[] bucket, int[][] sigArray) {
+		for(int i = 0; i < 210519; i++) {
+			int bucketValue = prependSignal(start, end, i, sigArray);
+			bucketValue = Math.abs(bucketValue);
+			bucket[bucketValue].addElement(i);
+		}
+		System.out.println("Bucket " + (start/5) + " done");
+		return bucket;
 	}
 	
 	public static int prependSignal(int start, int end, int column, int[][] sig) {
@@ -292,11 +163,13 @@ public class MillionSongs_Jaccard_Similarity {
 				else {
 					intersect++;
 				}
+			}
+			else {
 				union++;
 			}
 
 		}
-		similarity = intersect/(double)union;
+		similarity = (double)intersect/(intersect+union);
 		if(similarity >= 0.95) {
 			return true;
 		}
